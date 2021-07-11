5b80dd7b334a6afd271d529da552a4c52cb84be6
Amin Bigdeli <46269242+aminbigdeli@users.noreply.github.com>
2021-07-10 22:43:58 -0400

    Create inference.sh

---
+++ inference.sh
@@ -0,0 +1,10 @@
+#Define the path of each parameter
+CUDA_VISIBLE_DEVICES=1 \
+python inference.py \
+     -model bert-base-uncased \
+     -test ${test_path} \
+     -checkpoint ${checkpoint_path} \
+     -res ${result_path} \
+     -num_labels 3 \
+     -max_sequence_len 64 \
+     -test_batch_size 16 \
